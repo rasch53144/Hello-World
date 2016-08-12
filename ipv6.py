7d1fab74f844eb8d7ae2b5b228a60a32be587b49
Danny Rasch <rasch53144@gmail.com>
2016-08-12 15:10:55 -0500

    Class 1 of Keith Byers Python Class
    Added remarks and changed the period to colon when rejoining the address

--- ipv6.py
+++ ipv6.py
@@ -1,9 +1,11 @@
 
 ipv6 = 'FE80:0000:0000:0000:0101:A3EF:EE1E:1719'
-ipv6split = ipv6.split(":")
+ipv6split = ipv6.split(":") #split string at colon#
+                            #pass to variable ipv6split#
 
 
-ipv6join = ".".join(ipv6split)
+ipv6join = ":".join(ipv6split) #join string using a period#
+                                #variable is ipv6join*
 
 print '''We typed in an ipv6 address of
 ... FE80:0000:0000:0000:0101:A3EF:EE1E:1719'''
@@ -20,7 +22,7 @@ print ipv6join
 print "Is 0101 in eith file?  True or False?"
 print " "
 a = '0101'
-b = a in ipv6split
+b = a in ipv6split   #test equation#
 print b
-c = a in ipv6join
+c = a in ipv6join     #test equation#
 print c
